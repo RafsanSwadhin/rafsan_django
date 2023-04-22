582ea17d029382845862f3892360b0216c725c2b
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2023-04-22 20:21:41 +0600

    First Comment

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(index):
+    return HttpResponse('This is home')
+
+def blog(index):
+    return HttpResponse('This is blog')
